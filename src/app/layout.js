c5baab04a7456d54836a904b2c40c4075e52cb3c
Milan Thapa <thapamilan9762@gmail.com>
2025-03-05 18:46:56 +0545

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Milan Thapa | Portfolio website",
+  description: "I’m Milan Thapa, a full-stack developer from Kathmandu, Nepal, with a Bachelor’s in Computer Science and Information Technology from Tribhuvan University. I specialize in both front-end and back-end development and have a strong background in web and graphic design. Passionate about creating user-friendly experiences, I enjoy tackling complex challenges and continuously enhancing my skills.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
